4e4ffa695f331c58a8c38b66f11f12fd7a5b559f
jayachandrareddy2702 <122256738+jayachandrareddy2702@users.noreply.github.com>
2023-12-19 09:47:45 +0530

    Add files via upload

--- halfDiamond.java
+++ halfDiamond.java
@@ -2,28 +2,37 @@ import java.io.*;
 import java.util.*;
 class halfDiamond
 {
-   public static void main(String[]args)
-   {
-      int n,i,j;
-      Scanner sc = new Scanner(System.in);
-      System.out.println("enter the no .of rows: ");
-      n=sc.nextInt();
-      for(i=0;i<n;i++)
-      {
-         for(j=0;j<=i;j++)
+  public static void main(String[] args)
+  {
+     Scanner sc = new Scanner(System.in);
+     System.out.println("enter the no.of rows: ");
+     int rows = sc.nextInt();
+     int i,j,k;
+     for(i=1;i<=rows;i++)
+     {
+         for(j=rows-1;j>=i;j--)
          {
-            System.out.print("1 ");
+            System.out.print(" ");
          }
-        System.out.println();
-      }
-      for(i=1;i<n;i++)
-      {
-         for(j=n-i;j>=1;j--)
+         for(k=0;k<i;k++)
          {
-              System.out.print("1 ");
+            System.out.print("*");
          }
-         System.out.println();
-      }
-   }
+       System.out.println();
+    }
+    for(i=rows-1;i>=0;i--)
+    {
+         for(j=rows-1;j>=i;j--)
+         {
+            System.out.print(" ");
+         }
+         for(k=i;k>=1;k--)
+         {
+            System.out.print("*");
+         }
+      System.out.println();
+    }
+    
+  }
 }
-
+    
